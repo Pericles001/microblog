520ba21d095b4b182d30d5dd9d74a5d94c68ab3a
Pericles001 <periclesadjovi@outlook.fr>
2021-06-25 10:56:21 +0100

    add fake  posts to routes file

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,14 @@ from app import app, request, render_template
 @app.route('/index')
 def index():
     user = {'username':'Joe'}
+    posts = [
+        {
+        'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        }
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
     return render_template('index.html', title='Home', user=user)
