c9dac03abf9a344541f09f4eb28752ce7da0c740
pericles001 <periclesadjovi@outlook.fr>
2022-04-23 02:56:43 +0100

    Started debugging

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
-from flask import Flask, request, render_template
-from config import Config
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask import Flask
 from flask_login import LoginManager
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
 from app import routes, models, errors
+from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,4 +13,6 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from app import routes, models
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,12 @@
-from app import app, db
-from flask import render_template, flash, redirect, request, url_for
-from app.forms import LoginForm, RegistrationForm, EditProfileForm
-from flask_login import current_user, login_user, logout_user, login_required
-from app.models import User
-from werkzeug.urls import url_parse
 from datetime import datetime
 
+from flask import render_template, flash, redirect, request, url_for
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
 
-
-
+from app import app, db
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
+from app.models import User
 
 
 @app.before_request
@@ -16,6 +14,7 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+
 
 @app.route('/')
 @app.route('/index')
@@ -50,16 +49,16 @@ def edit_profile():
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title='Edit Profile', form=form)
 
+
 @app.route('/user/<username>')
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     posts = [
         {'author': user, 'body': 'Test post #1'},
-        {'author':user, 'body':'Test post #2'}
+        {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
-
 
 
 @app.route('/login', methods=['GET', 'POST'])
